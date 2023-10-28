df9757d2eb3a1cf9a2a3a774fd1a1229636090f2
Aithaba Youness <Youness.Aithaba@emsi-edu.ma>
2023-10-28 16:50:30 +0100

    final bank account with graphql

--- src/main/java/org/sid/bankaccountservice/entities/Customer.java
+++ src/main/java/org/sid/bankaccountservice/entities/Customer.java
@@ -1,5 +1,6 @@
 package org.sid.bankaccountservice.entities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,5 +18,6 @@ public class Customer {
     private Long id;
     private String name;
     @OneToMany(mappedBy = "customer")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private List<BankAccount>bankAccounts;
 }
